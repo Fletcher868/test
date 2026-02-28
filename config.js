13e1ece8959ee0558d45f04548e9bd7f70c21ad5
Fletcher868 <isoinst@proton.me>
2026-02-28 12:03:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
 export const PB_URL = 'https://pam-unhideous-chastenedly.ngrok-free.dev/';
+
+// PayPal Plan IDs
+export const PAYPAL_CLIENT_ID = 'AZ1FwPWxMIw_G1FmfV6ERfmMGXNWgLW9oY5zNeOgUtuz4zd2BgYrb62uWn7F3uPslGTtv31-yY5GNVBG';
+export const PLAN_MONTHLY = 'P-13C81424AE992050NNEW6Q4Y';
+export const PLAN_YEARLY  = 'P-8EL806812V1740904NEXUFOI';
